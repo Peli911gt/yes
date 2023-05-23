4f59d1b6a3b23c292df6d2ede683ed4a85f71d37
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-23 15:46:33 +0100

    Create chatbot_app.py

---
+++ chatbot_app.py
@@ -0,0 +1,55 @@
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+import streamlit as st
+
+# Load the text file and preprocess the data
+with open('moby_dick.txt', 'r', encoding='utf-8') as f:
+    data = f.read().replace('\n', ' ')
+
+# Tokenize the text into sentences
+sentences = sent_tokenize(data)
+
+# Define a function to preprocess each sentence
+def preprocess(sentence):
+    words = word_tokenize(sentence)
+    words = [word.lower() for word in words if word.lower() not in stopwords.words('english') and word not in string.punctuation]
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    return words
+
+# Preprocess each sentence in the text
+corpus = [preprocess(sentence) for sentence in sentences]
+
+# Define a function to find the most relevant sentence given a query
+def get_most_relevant_sentence(query):
+    query = preprocess(query)
+    max_similarity = 0
+    most_relevant_sentence = ""
+    for sentence in corpus:
+        similarity = len(set(query).intersection(sentence)) / float(len(set(query).union(sentence)))
+        if similarity > max_similarity:
+            max_similarity = similarity
+            most_relevant_sentence = " ".join(sentence)
+    return most_relevant_sentence
+
+# Define the chatbot function
+def chatbot(question):
+    most_relevant_sentence = get_most_relevant_sentence(question)
+    return most_relevant_sentence
+
+# Create a Streamlit app
+def main():
+    st.title("Chatbot")
+    st.write("Hello! I'm a chatbot. Ask me anything about Moby Dick.")
+    question = st.text_input("You:")
+    if st.button("Submit"):
+        response = chatbot(question)
+        st.write("Chatbot:", response)
+
+if __name__ == "__main__":
+    main()
