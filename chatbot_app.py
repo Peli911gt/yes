5e937fff0c1a59c5fe50fc0b929d213e4b489643
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-24 15:58:16 +0100

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,51 +1,47 @@
 import nltk
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
-from nltk.probability import FreqDist
+from nltk.stem import WordNetLemmatizer
 import string
 import streamlit as st
 
 # Load the text file and preprocess the data
 with open('moby_dick.txt', 'r', encoding='utf-8') as f:
     data = f.read().replace('\n', ' ')
-
 # Tokenize the text into sentences
 sentences = sent_tokenize(data)
 
-# Preprocess each sentence in the text
-def preprocess_sentence(sentence):
+# Define a function to preprocess each sentence
+def preprocess(sentence):
     # Tokenize the sentence into words
     words = word_tokenize(sentence)
     # Remove stopwords and punctuation
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words('english')) - {'and', 'his', 'in', 'the', 'a'}
     words = [word.lower() for word in words if word.lower() not in stop_words and word not in string.punctuation]
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
     return words
 
 # Preprocess each sentence in the text
-corpus = [preprocess_sentence(sentence) for sentence in sentences]
+corpus = [preprocess(sentence) for sentence in sentences]
 
 # Define a function to find the most relevant sentence given a query
 def get_most_relevant_sentence(query):
     # Preprocess the query
-    query = preprocess_sentence(query)
+    query = preprocess(query)
     # Compute the similarity between the query and each sentence in the text
     max_similarity = 0
     most_relevant_sentence = ""
     for sentence in corpus:
-        common_words = set(query).intersection(sentence)
-        if len(sentence) > 0:
-            # Calculate the frequency distribution of common words in the sentence
-            sentence_freq_dist = FreqDist(sentence)
-            # Calculate the relevance score by summing the frequencies of common words
-            relevance_score = sum([sentence_freq_dist[word] for word in common_words])
-            # Normalize the relevance score by the length of the sentence
-            similarity = relevance_score / float(len(sentence))
-            if similarity > max_similarity:
-                max_similarity = similarity
-                most_relevant_sentence = " ".join(sentence)
-    return most_relevant_sentence
+        similarity = len(set(query).intersection(sentence)) / float(len(set(query).union(sentence)))
+        if similarity > max_similarity:
+            max_similarity = similarity
+            most_relevant_sentence = sentence
+    return " ".join(most_relevant_sentence)
 
 # Define the chatbot function
 def chatbot(question):
