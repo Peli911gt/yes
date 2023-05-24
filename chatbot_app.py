a8e5affb6e2a8ad3d0202660fbb2938746835230
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-24 15:05:33 +0100

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,11 +1,8 @@
 import nltk
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
-nltk.download('wordnet')
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 import string
 import streamlit as st
 
@@ -16,24 +13,22 @@ with open('moby_dick.txt', 'r', encoding='utf-8') as f:
 # Tokenize the text into sentences
 sentences = sent_tokenize(data)
 
-# Define a function to preprocess each sentence
-def preprocess(sentence):
+# Preprocess each sentence in the text
+def preprocess_sentence(sentence):
     # Tokenize the sentence into words
     words = word_tokenize(sentence)
     # Remove stopwords and punctuation
-    stop_words = set(stopwords.words('english')) - {'and', 'his', 'in', 'the', 'a'}
+    stop_words = set(stopwords.words('english'))
     words = [word.lower() for word in words if word.lower() not in stop_words and word not in string.punctuation]
-    # Lemmatize the words
-    lemmatizer = WordNetLemmatizer()
-    words = [lemmatizer.lemmatize(word) for word in words]
     return words
 
 # Preprocess each sentence in the text
-corpus = [preprocess(sentence) for sentence in sentences]
+corpus = [preprocess_sentence(sentence) for sentence in sentences]
 
+# Define a function to find the most relevant sentence given a query
 def get_most_relevant_sentence(query):
     # Preprocess the query
-    query = preprocess(query)
+    query = preprocess_sentence(query)
     # Compute the similarity between the query and each sentence in the text
     max_similarity = 0
     most_relevant_sentence = ""
@@ -45,7 +40,6 @@ def get_most_relevant_sentence(query):
             most_relevant_sentence = " ".join(sentence)
     return most_relevant_sentence
 
-
 # Define the chatbot function
 def chatbot(question):
     # Find the most relevant sentence
@@ -56,7 +50,7 @@ def chatbot(question):
 # Create a Streamlit app
 def main():
     st.title("Chatbot")
-    st.write("Hello! I'm a chatbot. Ask me anything about Moby Dick.")
+    st.write("Hello! I'm a chatbot. Ask me anything about the topic in Moby Dick.")
     # Get the user's question
     question = st.text_input("You:")
     # Create a button to submit the question
