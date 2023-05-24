9300425dc4aaa77a378b1d5fe8ea641a5b5bc6ed
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-24 17:42:39 +0100

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -53,7 +53,7 @@ def chatbot(question):
 # Create a Streamlit app
 def main():
     st.title("Chatbot")
-    st.write("Hello! I'm a chatbot. Ask me anything about the text.")
+    st.write("Hello! I'm a chatbot. Ask me anything about Moby Dick.")
     # Get the user's question
     question = st.text_input("You:")
     # Create a button to submit the question
