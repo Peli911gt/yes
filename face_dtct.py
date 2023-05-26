c75977db8dfb7e0ee33b712ff5fa19373f2d23a4
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-26 17:10:34 +0100

    Update face_dtct.py

--- face_dtct.py
+++ face_dtct.py
@@ -1,41 +1,51 @@
-import cv2
 import streamlit as st
-import os
-os.environ['LD_PRELOAD'] = '/usr/lib/x86_64-linux-gnu/libGL.so'
+import numpy as np
+from PIL import Image
+import cv2
 
-# Add instructions to the interface
-st.write("# Face Detection App")
-st.write("Upload an image and adjust the parameters to detect faces.")
+def main():
+    st.title("Face Detection App")
 
-# Load the Haar cascade xml file for face detection
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    # Add instructions
+    st.write("Upload an image and click the 'Detect Faces' button to detect faces.")
+    st.write("Adjust the parameters below to customize the face detection.")
 
-# Upload image file
-uploaded_file = st.file_uploader("Choose an image file", type=['jpg', 'jpeg', 'png'])
+    # Upload image file
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    # Read the image
-    image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+    if uploaded_file is not None:
+        # Load image
+        image = Image.open(uploaded_file)
+        image_array = np.array(image)
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Detect faces
+        min_neighbors = st.slider("minNeighbors", min_value=1, max_value=10, value=5, step=1)
+        scale_factor = st.slider("scaleFactor", min_value=1.1, max_value=2.0, value=1.5, step=0.1)
 
-    # Adjustable parameters
-    rect_color = st.color_picker("Rectangle Color", "#FF0000")
-    min_neighbors = st.slider("minNeighbors", 1, 10, 5)
-    scale_factor = st.slider("scaleFactor", 1.1, 2.0, 1.1)
+        # Create face cascade
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Perform face detection
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=scale_factor, minNeighbors=min_neighbors, minSize=(30, 30))
+        # Convert image to grayscale
+        gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
 
-    # Draw rectangles around the detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), rect_color, 2)
+        # Perform face detection
+        faces = face_cascade.detectMultiScale(gray_image, minNeighbors=min_neighbors, scaleFactor=scale_factor)
 
-    # Display the image with detected faces
-    st.image(image, channels="BGR")
+        # Get rectangle color from user
+        rect_color = st.color_picker("Rectangle Color", "#ff0000")
 
-    # Save the image with detected faces
-    if st.button("Save Image"):
-        cv2.imwrite("detected_faces.jpg", image)
-        st.success("Image saved successfully!")
+        # Draw rectangles around the detected faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image_array, (x, y), (x+w, y+h), rect_color, 2)
+
+        # Display the annotated image
+        st.image(image_array, caption='Annotated Image', use_column_width=True)
+
+        # Save the annotated image
+        save_button = st.button("Save Image")
+        if save_button:
+            cv2.imwrite("annotated_image.jpg", image_array)
+            st.success("Image saved successfully.")
+
+if __name__ == '__main__':
+    main()
