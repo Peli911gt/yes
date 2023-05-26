16a5551a49bd9377414e45f1a32c6841970910b5
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-26 17:17:55 +0100

    Update face_dtct.py

--- face_dtct.py
+++ face_dtct.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import face_recognition
 import cv2
 
 def main():
@@ -16,35 +17,29 @@ def main():
     if uploaded_file is not None:
         # Load image
         image = Image.open(uploaded_file)
-        image_array = np.array(image)
+
+        # Convert image to RGB
+        image_rgb = np.array(image.convert('RGB'))
 
         # Detect faces
-        min_neighbors = st.slider("minNeighbors", min_value=1, max_value=10, value=5, step=1)
-        scale_factor = st.slider("scaleFactor", min_value=1.1, max_value=2.0, value=1.5, step=0.1)
-
-        # Create face cascade
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-        # Convert image to grayscale
-        gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
-
-        # Perform face detection
-        faces = face_cascade.detectMultiScale(gray_image, minNeighbors=min_neighbors, scaleFactor=scale_factor)
+        face_locations = face_recognition.face_locations(image_rgb, model="hog")
 
         # Get rectangle color from user
         rect_color = st.color_picker("Rectangle Color", "#ff0000")
 
         # Draw rectangles around the detected faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image_array, (x, y), (x+w, y+h), rect_color, 2)
+        image_draw = image.copy()
+        draw = ImageDraw.Draw(image_draw)
+        for (top, right, bottom, left) in face_locations:
+            draw.rectangle(((left, top), (right, bottom)), outline=rect_color, width=2)
 
         # Display the annotated image
-        st.image(image_array, caption='Annotated Image', use_column_width=True)
+        st.image(image_draw, caption='Annotated Image', use_column_width=True)
 
         # Save the annotated image
         save_button = st.button("Save Image")
         if save_button:
-            cv2.imwrite("annotated_image.jpg", image_array)
+            image_draw.save("annotated_image.jpg")
             st.success("Image saved successfully.")
 
 if __name__ == '__main__':
