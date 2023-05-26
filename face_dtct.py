c6964be566c6ec78943ff6aeeed6b1acada63341
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-26 22:42:54 +0100

    Update face_dtct.py

--- face_dtct.py
+++ face_dtct.py
@@ -1,46 +1,50 @@
 import streamlit as st
-import numpy as np
-from PIL import Image
-import face_recognition
 import cv2
+import numpy as np
 
-def main():
-    st.title("Face Detection App")
+# Add instructions to the interface
+st.write("# Face Detection App")
+st.write("Upload an image and adjust the parameters to detect faces.")
 
-    # Add instructions
-    st.write("Upload an image and click the 'Detect Faces' button to detect faces.")
-    st.write("Adjust the parameters below to customize the face detection.")
+# File uploader for image selection
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    # Upload image file
-    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Read the uploaded image
+    image = np.array(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-    if uploaded_file is not None:
-        # Load image
-        image = Image.open(uploaded_file)
+    # Display the uploaded image
+    st.image(image, channels="BGR", caption="Uploaded Image")
 
-        # Convert image to RGB
-        image_rgb = np.array(image.convert('RGB'))
+    # Parameters for face detection
+    min_neighbors = st.slider("minNeighbors", 1, 10, 5)
+    scale_factor = st.slider("scaleFactor", 1.1, 2.0, 1.5)
 
-        # Detect faces
-        face_locations = face_recognition.face_locations(image_rgb, model="hog")
+    # Detect faces in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, minNeighbors=min_neighbors, scaleFactor=scale_factor)
 
-        # Get rectangle color from user
-        rect_color = st.color_picker("Rectangle Color", "#ff0000")
+    # Draw rectangles around the detected faces
+    rect_color = st.color_picker("Rectangle Color", "#FF0000")
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), rect_color, 2)
 
-        # Draw rectangles around the detected faces
-        image_draw = image.copy()
-        draw = ImageDraw.Draw(image_draw)
-        for (top, right, bottom, left) in face_locations:
-            draw.rectangle(((left, top), (right, bottom)), outline=rect_color, width=2)
+    # Display the image with detected faces
+    st.image(image, channels="BGR", caption="Detected Faces")
 
-        # Display the annotated image
-        st.image(image_draw, caption='Annotated Image', use_column_width=True)
+    # Save the image with detected faces
+    save_image = st.button("Save Image")
+    if save_image:
+        cv2.imwrite("detected_faces.jpg", image)
+        st.write("Image saved successfully.")
 
-        # Save the annotated image
-        save_button = st.button("Save Image")
-        if save_button:
-            image_draw.save("annotated_image.jpg")
-            st.success("Image saved successfully.")
+# Display the instructions
+st.markdown("## Instructions")
+st.write("- Upload an image using the file uploader.")
+st.write("- Adjust the 'minNeighbors' and 'scaleFactor' sliders to control face detection.")
+st.write("- Choose the color of the rectangles using the color picker.")
+st.write("- Click on 'Save Image' to save the image with detected faces.")
 
-if __name__ == '__main__':
-    main()
