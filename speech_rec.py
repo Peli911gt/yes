9dfc552e927371e1919e17e3451aa3733232f955
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-27 17:10:18 +0100

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -1,15 +1,15 @@
 import streamlit as st
+import speech_recognition as sr
 from pydub import AudioSegment
-import pyspeech
 
 def main():
     st.title("Speech Recognition App")
 
-    # Initialize the speech recognizer
-    recognizer = pyspeech.SpeechRecognizer()
+    # Initialize the recognizer
+    recognizer = sr.Recognizer()
 
     # Get available APIs
-    api_options = recognizer.get_available_apis()
+    api_options = ["Google", "Wit.ai", "IBM Watson"]
 
     # User input for API selection
     api = st.selectbox("Select Speech Recognition API", api_options)
@@ -28,8 +28,12 @@ def main():
         if audio.channels > 1:
             audio = audio.set_channels(1)
 
+        # Save audio to a temporary file
+        temp_file = "temp.wav"
+        audio.export(temp_file, format="wav")
+
         # Transcribe speech
-        text = recognizer.transcribe(audio.get_array_of_samples(), audio.frame_rate, api, language)
+        text = transcribe_audio(temp_file, api, language)
 
         # Display the transcribed text
         st.text("Transcribed Text:")
@@ -42,6 +46,30 @@ def main():
             if filename:
                 save_transcribed_text(text, filename)
                 st.success(f"Transcribed text saved to {filename}")
+
+        # Delete the temporary audio file
+        os.remove(temp_file)
+
+def transcribe_audio(audio_file, api, language):
+    # Open the audio file
+    with sr.AudioFile(audio_file) as source:
+        # Load audio data
+        audio_data = recognizer.record(source)
+
+        # Choose the appropriate API
+        if api == "Google":
+            text = recognizer.recognize_google(audio_data, language=language)
+        elif api == "Wit.ai":
+            wit_ai_api_key = "YOUR_WIT_AI_API_KEY"  # Replace with your Wit.ai API key
+            text = recognizer.recognize_wit(audio_data, key=wit_ai_api_key)
+        elif api == "IBM Watson":
+            ibm_watson_api_key = "YOUR_IBM_WATSON_API_KEY"  # Replace with your IBM Watson API key
+            ibm_watson_url = "YOUR_IBM_WATSON_URL"  # Replace with your IBM Watson API URL
+            text = recognizer.recognize_ibm(audio_data, username=ibm_watson_api_key, password=ibm_watson_url)
+        else:
+            text = ""
+
+    return text
 
 def save_transcribed_text(text, filename):
     with open(filename, "w") as file:
