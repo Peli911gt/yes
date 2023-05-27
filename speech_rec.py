afc8c5d32221bbcfa32b09b16d9c2602eb2f9099
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-27 16:59:26 +0100

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -1,58 +1,55 @@
 import streamlit as st
-import speech_recognition as sr
-
-def transcribe_speech(api, language):
-    r = sr.Recognizer()
-    
-    if api == "Google Speech Recognition":
-        recognizer = sr.Recognizer()
-        recognizer.energy_threshold = 4000
-        recognizer.pause_threshold = 0.6
-
-        with sr.Microphone() as source:
-            st.info("Speak now...")
-            audio = recognizer.listen(source)
-            st.info("Transcribing...")
-
-        try:
-            text = recognizer.recognize_google(audio, language=language)
-            return text
-        except sr.UnknownValueError:
-            return "Sorry, I could not understand you."
-        except sr.RequestError:
-            return "Sorry, there was an issue with the speech recognition service."
-
-    elif api == "CMU Sphinx":
-        recognizer = sr.Recognizer()
-        recognizer.energy_threshold = 4000
-        recognizer.pause_threshold = 0.6
-
-        with sr.Microphone() as source:
-            st.info("Speak now...")
-            audio = recognizer.listen(source)
-            st.info("Transcribing...")
-
-        try:
-            text = recognizer.recognize_sphinx(audio)
-            return text
-        except sr.UnknownValueError:
-            return "Sorry, I could not understand you."
-        except sr.RequestError:
-            return "Sorry, there was an issue with the speech recognition service."
+import sounddevice as sd
+import pyspeech
 
 def main():
     st.title("Speech Recognition App")
-    st.write("Click on the microphone to start speaking:")
-    
-    available_apis = ["Google Speech Recognition", "CMU Sphinx"]
-    api = st.selectbox("Select Speech Recognition API", available_apis)
-    
-    available_languages = ["en-US", "es-ES"]  # Add more languages as needed
-    language = st.selectbox("Select Language", available_languages)
-    
-    if st.button("Start Recording"):
-        text = transcribe_speech(api, language)
-        st.write("Transcription:", text)
+
+    # Initialize the speech recognizer
+    recognizer = pyspeech.SpeechRecognizer()
+
+    # Get available APIs
+    api_options = recognizer.get_available_apis()
+
+    # User input for API selection
+    api = st.selectbox("Select Speech Recognition API", api_options)
+
+    # User input for language selection
+    language = st.text_input("Enter the language you are speaking in")
+
+    # Transcribe speech
+    text = transcribe_speech(api, language)
+
+    # Display the transcribed text
+    st.text("Transcribed Text:")
+    st.text(text)
+
+    # Save transcribed text to a file
+    save_to_file = st.button("Save to File")
+    if save_to_file:
+        filename = st.text_input("Enter the filename")
+        if filename:
+            save_transcribed_text(text, filename)
+            st.success(f"Transcribed text saved to {filename}")
+
+def transcribe_speech(api, language):
+    fs = 44100  # Sample rate
+    duration = 5  # Recording duration in seconds
+
+    with sd.rec(int(duration * fs), samplerate=fs, channels=1, blocking=True):
+        st.text("Listening...")
+
+        audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+        sd.wait()
+
+    st.text("Transcribing...")
+    text = recognizer.transcribe(audio[:, 0], fs, api, language)
+
+    return text
+
+def save_transcribed_text(text, filename):
+    with open(filename, "w") as file:
+        file.write(text)
 
 if __name__ == "__main__":
     main()
