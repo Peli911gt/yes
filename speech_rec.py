7829e8dddbb1a6735360f0df6384a728f5e9eb84
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-27 16:29:04 +0100

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -1,61 +1,58 @@
 import streamlit as st
 import speech_recognition as sr
 
-
-def transcribe_speech(api, language, recognizer, audio_text):
-    try:
-        if api == "Google Cloud Speech-to-Text":
-            text = recognizer.recognize_google_cloud(audio_text)
-        elif api == "Wit.ai":
-            text = recognizer.recognize_wit(audio_text, key=WIT_API_KEY)
-        elif api == "IBM Watson":
-            text = recognizer.recognize_ibm(audio_text, username=IBM_USERNAME, password=IBM_PASSWORD)
-        elif api == "Microsoft Azure Speech Service":
-            text = recognizer.recognize_azure(audio_text, key=AZURE_API_KEY)
-        elif api == "Mozilla DeepSpeech":
-            text = recognizer.recognize_deepspeech(audio_text)
-        else:
-            text = "Invalid API selection"
-    except sr.UnknownValueError:
-        text = "Unable to recognize speech"
-    except sr.RequestError as e:
-        text = f"Error occurred during transcription: {str(e)}"
+def transcribe_speech(api, language):
+    r = sr.Recognizer()
     
-    return text
+    if api == "Google Speech Recognition":
+        recognizer = sr.Recognizer()
+        recognizer.energy_threshold = 4000
+        recognizer.pause_threshold = 0.6
 
+        with sr.Microphone() as source:
+            st.info("Speak now...")
+            audio = recognizer.listen(source)
+            st.info("Transcribing...")
 
-def save_text(text):
-    with open("transcription.txt", "w") as file:
-        file.write(text)
-    st.write("Transcription saved to file.")
+        try:
+            text = recognizer.recognize_google(audio, language=language)
+            return text
+        except sr.UnknownValueError:
+            return "Sorry, I could not understand you."
+        except sr.RequestError:
+            return "Sorry, there was an issue with the speech recognition service."
 
+    elif api == "CMU Sphinx":
+        recognizer = sr.Recognizer()
+        recognizer.energy_threshold = 4000
+        recognizer.pause_threshold = 0.6
+
+        with sr.Microphone() as source:
+            st.info("Speak now...")
+            audio = recognizer.listen(source)
+            st.info("Transcribing...")
+
+        try:
+            text = recognizer.recognize_sphinx(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Sorry, I could not understand you."
+        except sr.RequestError:
+            return "Sorry, there was an issue with the speech recognition service."
 
 def main():
     st.title("Speech Recognition App")
-    st.write("Select Speech Recognition API:")
-    api = st.selectbox("API", ["Google Cloud Speech-to-Text", "Wit.ai", "IBM Watson", "Microsoft Azure Speech Service", "Mozilla DeepSpeech"])
+    st.write("Click on the microphone to start speaking:")
     
-    language = st.text_input("Language", "")
-
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-
+    available_apis = ["Google Speech Recognition", "CMU Sphinx"]
+    api = st.selectbox("Select Speech Recognition API", available_apis)
+    
+    available_languages = ["en-US", "es-ES"]  # Add more languages as needed
+    language = st.selectbox("Select Language", available_languages)
+    
     if st.button("Start Recording"):
-        with microphone as source:
-            audio_text = recognizer.listen(source)
-
-        text = transcribe_speech(api, language, recognizer, audio_text)
+        text = transcribe_speech(api, language)
         st.write("Transcription:", text)
-
-        if st.button("Save Transcription"):
-            save_text(text)
-
-        if st.button("Pause"):
-            recognizer.pause_threshold = 999999999
-
-        if st.button("Resume"):
-            recognizer.pause_threshold = 0.8
-
 
 if __name__ == "__main__":
     main()
