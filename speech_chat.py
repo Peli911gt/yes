50ec748b7f893edca402324841f2968f0cb5a08a
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-30 01:34:58 +0100

    Update speech_chat.py

--- speech_chat.py
+++ speech_chat.py
@@ -1,14 +1,21 @@
 import nltk
 import streamlit as st
 import speech_recognition as sr
+import sounddevice as sd
+import numpy as np
 
 def transcribe_speech():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
+    fs = 44100
+    duration = 5  # Adjust the duration as needed
+
+    with sd.rec(int(duration * fs), samplerate=fs, channels=1):
         st.write("Speak something...")
-        audio = r.listen(source)
+        sd.wait()
 
     try:
+        audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+        audio = np.squeeze(audio)
         text = r.recognize_google(audio)
         return text
     except sr.UnknownValueError:
