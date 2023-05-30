9d8ea18f035bfd28b474ba39a506364046653b28
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-30 15:52:36 +0100

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -1,46 +1,17 @@
 import streamlit as st
-import speech_recognition as sr
-
-def chatbot(input_text):
-    # Simple chatbot logic for demonstration
-    if input_text.lower() == "hello":
-        response = "Hi there!"
-    else:
-        response = "I'm sorry, I didn't understand that."
-
-    return response
-
-def transcribe_speech():
-    r = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        st.write("Listening...")
-        audio = r.listen(source)
-
-    try:
-        text = r.recognize_google(audio)
-        return text
-    except sr.UnknownValueError:
-        return "Sorry, I could not understand audio."
-    except sr.RequestError:
-        return "Sorry, there was an issue with the speech recognition service."
 
 def main():
-    st.title("Speech-Enabled Chatbot")
-
-    # Get user input (speech or text)
-    option = st.radio("Input Type", ("Text", "Speech"))
-
-    if option == "Text":
-        text_input = st.text_input("Enter your message", "")
-    elif option == "Speech":
-        text_input = transcribe_speech()
-
-    # Process user input and generate response
+    st.title("Speech-enabled Chatbot")
+    
+    # Get user input
+    text_input = st.text_input("Enter your message", "")
+    
     if text_input:
+        # Call chatbot algorithm to generate response
         response = chatbot(text_input)
-        st.write("Chatbot:", response)
 
-# Run the app
+        # Display the chatbot response
+        st.text_area("Chatbot:", value=response, height=200)
+
 if __name__ == "__main__":
     main()
