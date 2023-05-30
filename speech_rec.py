489082fbddf44f17976582010f081d3455ab4b0b
Peli911gt <92979921+Peli911gt@users.noreply.github.com>
2023-05-30 15:47:04 +0100

    Update speech_rec.py

--- speech_rec.py
+++ speech_rec.py
@@ -1,50 +1,45 @@
 import streamlit as st
 import speech_recognition as sr
-from google.cloud import speech_v1p1beta1 as speech
 
-# Function to transcribe speech using Google Cloud Speech-to-Text API
+def chatbot(input_text):
+    # Simple chatbot logic for demonstration
+    if input_text.lower() == "hello":
+        response = "Hi there!"
+    else:
+        response = "I'm sorry, I didn't understand that."
+
+    return response
+
 def transcribe_speech():
-    # Configure speech recognition client
-    client = speech.SpeechClient()
-
-    # Start recording audio from the microphone
     r = sr.Recognizer()
+
     with sr.Microphone() as source:
-        st.write("Listening... Say something!")
+        st.write("Listening...")
         audio = r.listen(source)
 
-    # Transcribe the recorded audio using Google Cloud Speech-to-Text API
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
-    audio_data = speech.RecognitionAudio(content=audio.get_raw_data())
-    response = client.recognize(config=config, audio=audio_data)
+    try:
+        text = r.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        return "Sorry, I could not understand audio."
+    except sr.RequestError:
+        return "Sorry, there was an issue with the speech recognition service."
 
-    # Extract the transcribed text from the response
-    text = ""
-    for result in response.results:
-        text += result.alternatives[0].transcript
-
-    return text
-
-# Main function
 def main():
     st.title("Speech-Enabled Chatbot")
 
-    # Get user input (text or speech)
-    input_type = st.radio("Input Type", ("Text", "Speech"))
-    if input_type == "Text":
-        text_input = st.text_input("Enter your message")
-    else:
+    # Get user input (speech or text)
+    option = st.radio("Input Type", ("Text", "Speech"))
+
+    if option == "Text":
+        text_input = st.text_input("Enter your message", "")
+    elif option == "Speech":
         text_input = transcribe_speech()
 
-    # Call chatbot algorithm to generate response
-    response = chatbot(text_input)
-
-    # Display the chatbot response
-    st.write("Chatbot:", response)
+    # Process user input and generate response
+    if text_input:
+        response = chatbot(text_input)
+        st.write("Chatbot:", response)
 
 # Run the app
 if __name__ == "__main__":
